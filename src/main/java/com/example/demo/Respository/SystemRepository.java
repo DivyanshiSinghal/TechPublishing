9cbb4cb100584d80888291ceac2cc2d8216a4808
DivyanshiSinghal <divyanshisinghal95@gmail.com>
2019-04-22 14:48:10 +0530

    Update SystemRepository.java

--- src/main/java/com/example/demo/Respository/SystemRepository.java
+++ src/main/java/com/example/demo/Respository/SystemRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.Respository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.model.Admin.java;
+
+import com.example.demo.model.Admin;
+
 
 @Repository
 public interface SystemRepository extends CrudRepository<Admin,Long> {
