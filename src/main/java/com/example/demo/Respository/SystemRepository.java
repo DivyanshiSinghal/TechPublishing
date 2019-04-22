50800b14d78a3e634c3aca81f9b49524515f63f0
DivyanshiSinghal <divyanshisinghal95@gmail.com>
2019-04-22 13:39:01 +0530

    Update SystemRepository.java

--- src/main/java/com/example/demo/Respository/SystemRepository.java
+++ src/main/java/com/example/demo/Respository/SystemRepository.java
@@ -2,7 +2,7 @@ package com.michaelcgood.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import com.michaelcgood.model.SystemExample;
+import com/example/demo/model/Admin.java;
 
 @Repository
 public interface SystemRepository extends CrudRepository<SystemExample,Long> {
